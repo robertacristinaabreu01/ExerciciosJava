6c0a05c4dbe833fb72e0d389a628aec993ccad63
Roberta Abreu <76497778+robertacristinaabreu01@users.noreply.github.com>
2021-01-11 16:25:17 -0300

    Add files via upload

---
+++ TipoTexto.java
@@ -0,0 +1,18 @@
+  
+import java.util.Scanner;
+
+public class TipoTexto {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String nome = scanner.nextLine();
+		
+		System.out.println("Olá " + nome + "!");
+		
+		scanner.close();
+		
+		Character variavelChar = 'A';
+	}
+
+}
